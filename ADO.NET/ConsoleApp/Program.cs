0e60b1e2fe29f1f1e9947332c223453164ebb50f
Omelian Levkovych <omelianai@gmail.com>
2021-12-11 00:41:00 +0200

    Simple connection

---
+++ ADO.NET/ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = @"Server=.;Database=SandboxDb;Integrated Security=true;";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                Console.WriteLine("o");
+            }
+        }
+    }
+}
