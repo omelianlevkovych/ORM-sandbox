102a6769a0740cfda01207da22c1a773ca72eaed
Omelian Levkovych <omelianai@gmail.com>
2022-01-19 17:18:26 +0200

    connection open async

--- ADO.NET/ConsoleApp/Program.cs
+++ ADO.NET/ConsoleApp/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Data;
 using System.Data.SqlClient;
+using System.Diagnostics;
 
 namespace ConsoleApp
 {
@@ -7,13 +9,27 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            var connectionString = @"Server=.;Database=SandboxDb;Integrated Security=true;";
+            var connectionString = @"Server=.;Database=usersdb;Integrated Security=true;";
 
-            using (var connection = new SqlConnection(connectionString))
+            using var connection = new SqlConnection(connectionString);
+
+            var watch = new Stopwatch();
+            watch.Start();
+
+            connection.OpenAsync();
+            PrintConnectionState(connection);
+
+            watch.Stop();
+            Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
+        }
+
+        public static void PrintConnectionState(IDbConnection dbConnection)
+        {
+            while (dbConnection.State is not ConnectionState.Open)
             {
-                connection.Open();
-                Console.WriteLine("o");
+                Console.WriteLine("Connection is closed");
             }
+            Console.WriteLine("Connection is open");
         }
     }
 }
