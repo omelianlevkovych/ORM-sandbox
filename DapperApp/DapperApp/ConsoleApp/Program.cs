aeb6e719e1f9b9d6302f2162f6ef726164db1366
Omelian Levkovych <omelianai@gmail.com>
2022-01-27 17:35:20 +0200

    Added simplest solution

---
+++ DapperApp/DapperApp/ConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using ConsoleApp.Models;
+using System;
+using System.Data;
+using Dapper;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = @"Server=.;Database=usersdb;Integrated Security=true;";
+            using IDbConnection db = new SqlConnection(connectionString);
+
+            var persons = db.Query<Person>("SELECT * FROM Persons").ToList();
+
+            foreach (Person p in persons)
+            {
+                Console.WriteLine($"{p.Id} | {p.FirstName} | {p.LastName} | {p.Age}");
+            }
+
+            Console.Read();
+        }
+    }
+}
